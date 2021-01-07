6962f262af984baa5ec9c6c84dcfc69356e610f9
traditionallimb <traditionallimb@users.noreply.github.com>
2021-01-07 20:22:10 +0000

    client

--- src/structures/traditionalclient.js
+++ src/structures/traditionalclient.js
@@ -1,4 +1,5 @@
 const { Client } = require('discord.js');
+const ENV = require('.env');
 
 module.exports = class traditionalclient extends Client {
 
@@ -12,6 +13,15 @@ module.exports = class traditionalclient extends Client {
       console.log(`Logged in as ${this.user.username}`);
     });
 
-    this.on('message', async (message))
+    this.on('message', async (message) => {
+      const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
+      const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
+
+      if(!message.guild || message.author.bot) return;
+
+      if(message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${ENV.PREFIX}\`.`);
+
+      const prefix
+    })
   }
 }
