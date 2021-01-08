a9044f20cf4f5ae2ab6e8b5491f0e57886a58af9
traditionallimb <traditionallimb@users.noreply.github.com>
2021-01-08 12:43:41 +0000

    made basic bot stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const traditionalclient = require('./structures/traditionalclient');
-const config = require('../.env');
+const config = require('../config.json');
 
+// eslint-disable-next-line new-cap
 const client = new traditionalclient(config);
-client.login()
+client.login();
--- src/structures/traditionalclient.js
+++ src/structures/traditionalclient.js
@@ -1,27 +1,50 @@
 const { Client } = require('discord.js');
-const ENV = require('.env');
 
 module.exports = class traditionalclient extends Client {
 
-  constructor(options = {}) {
-    super({
-      disableMentions: 'everyone'
-    });
-    this.validate(options);
+	constructor(options = {}) {
+		super({
+			disableMentions: 'everyone'
+		});
+		this.validate(options);
 
-    this.once('ready', () => {
-      console.log(`Logged in as ${this.user.username}`);
-    });
+		this.once('ready', () => {
+			console.log(`Logged in as ${this.user.username}`);
+		});
 
-    this.on('message', async (message) => {
-      const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
-      const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
+		this.on('message', async (message) => {
+			const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
+			const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
 
-      if(!message.guild || message.author.bot) return;
+			if (!message.guild || message.author.bot) return;
 
-      if(message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${ENV.PREFIX}\`.`);
+			if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${ENV.PREFIX}\`.`);
 
-      const prefix
-    })
-  }
-}
+			const prefix = message.content.match(mentionRegexPrefix) ?
+				message.content.match(mentionRegexPrefix)[0] : this.prefix;
+
+			// eslint-disable-next-line no-unused-vars
+			const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+
+			if (cmd.toLowerCase() === 'hello') {
+				message.channel.send('Hai!');
+			}
+		});
+	}
+
+	validate(options) {
+		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object');
+
+		if (!options.token) throw new Error('You must pass the token for the client.');
+		this.token = options.token;
+
+		if (!options.prefix) throw new Error('You must pass a prefix for the client');
+		if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of string');
+		this.prefix = options.prefix;
+	}
+
+	async login(token = this.token) {
+		super.login(token);
+	}
+
+};
