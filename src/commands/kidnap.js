ee04fcdc21dd5187a3cfdb63df0992eb727d982f
traditionallimb <traditionallimb@users.noreply.github.com>
2021-01-22 11:37:09 +0000

    kidnap

---
+++ src/commands/kidnap.js
@@ -0,0 +1,16 @@
+const command = require('./../structures/command.js');
+
+module.exports = class extends command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pog', 'pogger', 'pogging']
+		});
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	async run(message, args) {
+		message.channel.send('<:kidnap:796496816073736274>');
+	}
+
+};
