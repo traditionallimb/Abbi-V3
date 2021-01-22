1bfd14a4a96915c870874840d8b53d1d6a4305f9
traditionallimb <traditionallimb@users.noreply.github.com>
2021-01-22 12:02:38 +0000

    angy

---
+++ src/commands/angy.js
@@ -0,0 +1,16 @@
+const command = require('./../structures/command.js');
+
+module.exports = class extends command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pog', 'pogger', 'pogging']
+		});
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	async run(message, args) {
+		message.channel.send('<:angy:789917320973385738>');
+	}
+
+};
